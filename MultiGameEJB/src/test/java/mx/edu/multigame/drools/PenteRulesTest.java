60cd1a94f34c33c9d55d89384ad756984f41f7b9
Andrew Waterman <andrew.waterman@gmail.com>
2008-10-20 20:45:21 -0500

    Revert "Revert "Modifications to allow for trias and tesseras to both be counted when on the same vertice.""
    This reverts commit 214b2d2a3f372961ba4d496b783026cffa6f83e9.

--- MultiGameEJB/src/test/java/mx/edu/multigame/drools/PenteRulesTest.java
+++ MultiGameEJB/src/test/java/mx/edu/multigame/drools/PenteRulesTest.java
@@ -355,8 +355,12 @@ public class PenteRulesTest extends RulesTestBase {
 		
 		Set<BeadString> trias = move.getTrias();
 		Set<BeadString> tesseras = move.getTesseras();
+		
+		/* Tests the case that a tria and a tessera happen on the
+		 * same Vertice in the same move.
+		 */
 		assertEquals (1, tesseras.size());
-		assertEquals (0, trias.size());
+		assertEquals (1, trias.size());
 	}
 
 	@Test
