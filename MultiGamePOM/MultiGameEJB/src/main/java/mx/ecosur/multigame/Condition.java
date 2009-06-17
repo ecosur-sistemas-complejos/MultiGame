5611b0c9cb41db5dfab7bc27c403be3f43850ea3
Andrew Waterman <andrew.waterman@gmail.com>
2009-06-17 10:53:52 -0500

    Added Condition and Agent interfaces to source control.

---
+++ MultiGamePOM/MultiGameEJB/src/main/java/mx/ecosur/multigame/Condition.java
@@ -0,0 +1,17 @@
+/*
+* Copyright (C) 2008, 2009 ECOSUR, Andrew Waterman
+*
+* Licensed under the Academic Free License v. 3.2.
+* http://www.opensource.org/licenses/afl-3.0.php
+*/
+
+/**
+ * @author awaterma@ecosur.mx
+ */
+package mx.ecosur.multigame;
+
+import java.io.Serializable;
+
+public interface Condition extends Serializable {
+
+}
